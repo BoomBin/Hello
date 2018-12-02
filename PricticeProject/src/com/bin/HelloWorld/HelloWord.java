9771279704e6290c1aa3fc2f5bbefd9ddbb3f8dc
wang <jianbo@wangs-MacBook-Pro.local>
2018-12-02 17:59:40 +0800

    第一次改动
    第一次改动

--- PricticeProject/src/com/bin/HelloWorld/HelloWord.java
+++ PricticeProject/src/com/bin/HelloWorld/HelloWord.java
@@ -3,5 +3,7 @@ package com.bin.HelloWorld;
 public class HelloWord {
 	public static void main(String[] args) {
 		System.out.println("HelloWord");
+		System.out.println("hello Github");
+		System.out.println("你能看到我的改动吗？");
 	}
 }
